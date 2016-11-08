331680dbe8453979457814ee419a7520ba013f86
pawan-s <lordprometheus@Lords-MacBook-Pro.local>
2016-11-08 00:05:20 -0500

    Add personalize page

--- my-app.js
+++ my-app.js
@@ -18,6 +18,10 @@ var mainView = myApp.addView('.view-main', {
 var view1 = myApp.addView('#view-1');
 var view2 = myApp.addView('#view-2');
 var view3 = myApp.addView('#view-3');
+var view4 = myApp.addView('#view-4', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
 
 
 // Onboarding Page
